0048c325009590e3caf1c1367b951d77cd6152fd
ccyjw8860 <ccyjw8860@gmail.com>
2023-06-24 18:14:10 +0900

    1분 데이터 반환
    Query로 code, start_date, end_date를 보내면 localhost:8000/1min/ 에서 데이터 get할 수 있게 코드 update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 import sqlite3
 import json
 from fastapi.middleware.cors import CORSMiddleware
+import datetime
+import pandas as pd
 
 app = FastAPI()
 
@@ -22,6 +24,11 @@ app.add_middleware(
 
 def get_day_db_connection():
     conn = sqlite3.connect('./db/stock_price(day).db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_1min_db_connection():
+    conn = sqlite3.connect('./db/stock_price(1min).db')
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -53,6 +60,65 @@ async def read_table(table_name: str, date: int):
                 day_conn.close()
                 raise HTTPException(status_code=404, detail="Table not found")
 
+    return result
+
+def refine_row(row):
+    dateTime, Open, High, Low, Close, Volume = row
+    dateTime = str(dateTime)
+    Date = dateTime[:8]
+    Time = dateTime[8:]
+
+    return (int(Date), int(Time), int(Open), int(High), int(Low), int(Close), int(Volume))
+
+@app.get("/1min/")
+async def read_table(code: str, start_date: int, end_date:int):
+    start = datetime.datetime.now()
+    df = pd.DataFrame(columns=['Date','Time','Open','High','Low','Close','Amount'])
+    result = {}
+    # date must be in the YYYYMMDD format
+    if len(str(start_date)) != 8 and len(str(end_date) != 8):
+        raise HTTPException(status_code=400, detail="Invalid date format")
+ 
+    conn = get_1min_db_connection()
+    cursor = conn.cursor()
+
+    # Get all table names in the database
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = [row[0] for row in cursor.fetchall()]
+
+    # If the requested table is not in the list of tables, return an error
+    if f"A{code}" in tables:
+        if len(str(start_date)) == 8 and len(str(end_date)) == 8:
+            try:
+                # cursor.execute(f"SELECT * FROM A{table_name} WHERE Date >= 20230601 AND Date <= {str(date)};")
+                cursor.execute(f"SELECT * FROM A{code}")
+                rows = cursor.fetchall()
+                rows = list(map(lambda x: refine_row(x), rows))
+                rows = list(filter(lambda x: start_date <= x[0] <= end_date, rows))
+                # Convert rows into dict format for returning as JSON
+                # rows = list(filter(lambda x: start_date <= x[0] <= end_date, rows))
+                for r in rows:
+                    data = {
+                        'Time':r[1],
+                        'Open':r[2],
+                        'High':r[3],
+                        'Low':r[4],
+                        'Close':r[5],
+                        'Amount':r[6]
+                    }
+                    try:
+                        result[r[0]].append(data) 
+                    except KeyError as e:
+                        result[r[0]] = []
+                        result[r[0]].append(data)
+
+                conn.close()
+            except sqlite3.OperationalError:  # if the table doesn't exist
+                conn.close()
+                raise HTTPException(status_code=404, detail="Table not found")
+    end = datetime.datetime.now()
+    executed_time = end-start
+    print(executed_time)
     return result
 
 def loadJson(path):
